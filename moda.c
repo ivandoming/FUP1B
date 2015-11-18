1ba5ba3f3d3555d610f23a544a75884202d1e0b8
ivandoming <ivan_dominguez_barroso@hotmail.com>
2015-11-18 01:39:50 -0600

    no lo complete me atraso una expocicion que tenia

---
+++ moda.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int i;
+int suma;
+int calificaciones;
+int moda[10];
+int main()
+{
+		for(i=0;i<10;i++)
+	{
+		scanf("%d",&calificaciones);
+		
+		moda[calificaciones["i"]-1]++;
+	}
+	
+	moda[10]/=calificaciones;
+	printf("promedio%d\n",moda);
+	
+	printf("moda%d\n",calificaciones);
+
+}
